6075352336a5e4ba0ea0b1b864af744a65ce0c44
Kevin Xiao <aquarius.c.c.a@gmail.com>
2025-07-24 14:03:47 +0800

    chore: 配置 pinia 持久化插件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
+
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from "./App.vue";
 import router from "./router";
@@ -39,7 +41,9 @@ if (import.meta.env.DEV) {
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia(); 
+pinia.use(piniaPluginPersistedstate); // 引入持久化插件
+app.use(pinia);
 app.use(router);
 app.use(ElementPlus, {
   locale: zhTw,
