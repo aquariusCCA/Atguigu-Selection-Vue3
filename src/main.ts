282424c477162a502a1144c31b9eb1a61cbe82e8
Kevin Xiao <aquarius.c.c.a@gmail.com>
2025-07-24 09:59:06 +0800

    chore: 安裝所需的依賴

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhTw from "element-plus/es/locale/lang/zh-tw";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus, {
+  locale: zhTw,
+});
+
+app.mount("#app");
